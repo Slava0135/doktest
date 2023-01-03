847c9300aed79d17f31bb31b67516c59587cd8c8
slava0135 <super.novalskiy_0135@inbox.ru>
2023-01-03 12:24:53 +0300

    extract docs (basic)

--- src/main/kotlin/doktest/extractor/extractor.kt
+++ src/main/kotlin/doktest/extractor/extractor.kt
@@ -3,3 +3,37 @@ package doktest.extractor
 fun extractPackage(text: String): String? {
     return text.split(" ").getOrNull(1)
 }
+
+const val docStart = "/**"
+const val docMid = "*"
+const val docEnd = "*/"
+
+data class Doc(val content: String, val lines: IntRange)
+
+fun extractDocs(text: String): List<Doc> {
+    val lines = text.lines()
+    val result = mutableListOf<Doc>()
+    var startIndex = 1
+    var isDoc = false
+    for (i in lines.indices) {
+        val lineNumber = i + 1
+        val line = lines[i]
+        if (isDoc) {
+            if (line.trimStart().startsWith(docEnd)) {
+                val endIndex = lineNumber
+                val content = lines.slice(startIndex - 1..endIndex - 1).joinToString("\n")
+                result.add(Doc(content, startIndex..endIndex))
+                continue
+            }
+            if (!line.trimStart().startsWith(docMid) && line.isNotBlank()) {
+                isDoc = false
+            }
+        } else {
+            if (line.trimStart().startsWith(docStart)) {
+                startIndex = lineNumber
+                isDoc = true
+            }
+        }
+    }
+    return result
+}
--- src/test/kotlin/doktest/extractor/ExtractorKtTest.kt
+++ src/test/kotlin/doktest/extractor/ExtractorKtTest.kt
@@ -17,4 +17,21 @@ class ExtractorKtTest {
     fun `test extract package with failure`() {
         assertEquals(null, extractPackage(""))
     }
+
+    @Test
+    fun `test extract docs with 1 comment`() {
+        val comment = """
+            |/**
+            | * boo far
+            | */
+        """.trimMargin()
+        val input = comment + """
+            |
+            |fun foobar() {
+            |   println("foobar")
+            |}
+        """.trimMargin()
+        val expect = listOf(Doc(comment.trimMargin(), 1..3))
+        assertEquals(expect, extractDocs(input))
+    }
 }
