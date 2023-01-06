56f0d31d1aec7bda60160266d9f0cd165a867861
slava0135 <super.novalskiy_0135@inbox.ru>
2023-01-06 10:56:16 +0300

    helpers

--- src/main/kotlin/doktest/extractor/extractor.kt
+++ src/main/kotlin/doktest/extractor/extractor.kt
@@ -1,33 +1,5 @@
 package doktest.extractor
 
-import doktest.extractor.helpers.Doc
-
 fun extractPackage(text: String): String? {
     return text.split(" ").getOrNull(1)
-}
-
-data class RawDocTest(val content: List<String>, val lineNumbers: IntRange)
-
-fun extractRawDocTests(doc: Doc): List<RawDocTest> {
-    val result = mutableListOf<RawDocTest>()
-    var isDocTest = false
-    var startIndex = 0
-    for ((relLineNumber, line) in doc.content.withIndex()) {
-        if (!isDocTest) {
-            if (line.startsWith("```kotlin doctest")) {
-                isDocTest = true
-                startIndex = relLineNumber
-            }
-        } else {
-            if (line.startsWith("```")) {
-                val offset = doc.lineNumbers.first
-                result.add(RawDocTest(
-                    doc.content.slice(startIndex + 1 until relLineNumber),
-                    startIndex + offset..relLineNumber + offset
-                ))
-                isDocTest = false
-            }
-        }
-    }
-    return result
 }
