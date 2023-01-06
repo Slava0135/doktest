cbc367866860fe31ff049eb1588f4d1ffaaa813f
slava0135 <super.novalskiy_0135@inbox.ru>
2023-01-06 11:14:02 +0300

    extract all raw doc tests

--- src/main/kotlin/doktest/extractor/extractor.kt
+++ src/main/kotlin/doktest/extractor/extractor.kt
@@ -1,5 +1,18 @@
 package doktest.extractor
 
+import doktest.extractor.helpers.RawDocTest
+import doktest.extractor.helpers.extractDocs
+import doktest.extractor.helpers.extractRawDocTests
+
 fun extractPackage(text: String): String? {
     return text.split(" ").getOrNull(1)
+}
+
+fun extractAllRawDocTests(text: String): List<RawDocTest> {
+    val result = mutableListOf<RawDocTest>()
+    val docs = extractDocs(text)
+    for (doc in docs) {
+        result.addAll(extractRawDocTests(doc))
+    }
+    return result
 }
--- src/test/kotlin/doktest/extractor/ExtractorKtTest.kt
+++ src/test/kotlin/doktest/extractor/ExtractorKtTest.kt
@@ -1,5 +1,6 @@
 package doktest.extractor
 
+import doktest.extractor.helpers.*
 import kotlin.test.*
 
 class ExtractorKtTest {
@@ -18,6 +19,51 @@ class ExtractorKtTest {
     @Test
     fun `test extract package with failure`() {
         assertEquals(null, extractPackage(""))
+    }
+
+    @Test
+    fun `test extract all raw doc tests`() {
+        val input = """
+            |/** 0
+            | * ```kotlin doctest
+            | *     foobar()
+            | * ```
+            | */ 4
+            |fun foobar() {
+            |    println("foobar")
+            |}
+            |/** 8
+            | * ```kotlin
+            | *     println("h")
+            | * ```
+            | */ 12
+            |class boofar() {
+            |    /** 14
+            |     *  15
+            |     * ```kotlin doctest
+            |     * foobaz()
+            |     * foobaz()
+            |     * ```
+            |     *  20
+            |     *  21
+            |     *  22
+            |     * ```kotlin doctest
+            |     * foobaz()
+            |     * foobar()
+            |     * println("foobaz")
+            |     * ```
+            |     */ 28
+            |    fun foobaz() {
+            |       println("foobaz")
+            |    }
+            |}
+        """.trimMargin()
+        val expect = listOf(
+            RawDocTest(listOf("    foobar()"), 1..3),
+            RawDocTest(listOf("foobaz()", "foobaz()"), 16..19),
+            RawDocTest(listOf("foobaz()", "foobar()", "println(\"foobaz\")"), 23..27),
+        )
+        assertEquals(expect, extractAllRawDocTests(input))
     }
 
 }
