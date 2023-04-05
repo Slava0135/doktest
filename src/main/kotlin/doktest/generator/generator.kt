4ccdf6decffd6c4338dd73b5b014c6264e2cf590
Slava0135 <super.novalskiy_0135@inbox.ru>
2023-04-05 14:18:48 +0300

    nomain option testfile generation

--- src/main/kotlin/doktest/generator/generator.kt
+++ src/main/kotlin/doktest/generator/generator.kt
@@ -1,18 +1,31 @@
 package doktest.generator
 
+import doktest.extractor.Option
 import doktest.extractor.RawDocTest
 
 data class DocTest(val content: String, val lineNumbers: IntRange, val pkg: String)
 
 fun generateDocTest(doc: RawDocTest, pkg: String): DocTest {
     val imports = listOf("import $pkg.*", "import kotlin.test.*") + doc.content.filter { it.startsWith("import ") }
-    val main = doc.content.filter { it !in imports }
-    val content = """
-        |${imports.joinToString("\n")}
-        |
-        |fun main() {
-        |${main.joinToString("\n")}
-        |}
-    """.trimMargin()
+    val rawContent = doc.content.filter { it !in imports }
+    val content = when (doc.option) {
+        Option.RUN -> TODO()
+
+        Option.NORUN ->
+            """
+            |${imports.joinToString("\n")}
+            |
+            |fun main() {
+            |${rawContent.joinToString("\n")}
+            |}
+            """.trimMargin()
+
+        Option.NOMAIN ->
+            """
+            |${imports.joinToString("\n")}
+            |
+            |${rawContent.joinToString("\n")}
+            """.trimMargin()
+    }
     return DocTest(content, doc.lineNumbers, pkg)
 }
--- src/test/kotlin/doktest/generator/GeneratorKtTest.kt
+++ src/test/kotlin/doktest/generator/GeneratorKtTest.kt
@@ -1,7 +1,7 @@
 package doktest.generator
 
-import doktest.extractor.RawDocTest
 import doktest.extractor.Option
+import doktest.extractor.RawDocTest
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -43,7 +43,30 @@ class GeneratorKtTest {
         assertEquals(
             expect,
             generateDocTest(
-                RawDocTest(listOf("import bar.foo", "import foo.foo", "    foobaz()", "    bazfoo()"), -1..-1, Option.NORUN),
+                RawDocTest(
+                    listOf("import bar.foo", "import foo.foo", "    foobaz()", "    bazfoo()"),
+                    -1..-1,
+                    Option.NORUN
+                ),
+                "foo.bar"
+            ).content
+        )
+    }
+
+    @Test
+    fun `test generate doc test with nomain option`() {
+        val expect = """
+            |import foo.bar.*
+            |import kotlin.test.*
+            |
+            |class Foo {
+            |    val bar = 0
+            |}
+        """.trimMargin()
+        assertEquals(
+            expect,
+            generateDocTest(
+                RawDocTest(listOf("class Foo {", "    val bar = 0", "}"), -1..-1, Option.NOMAIN),
                 "foo.bar"
             ).content
         )
