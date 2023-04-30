2a480ee08f408505c65ddeab284acb2bb933a7ac
Slava0135 <super.novalskiy_0135@inbox.ru>
2023-04-30 10:17:27 +0300

    DoktestPlugin refactoring

--- src/main/kotlin/doktest/DoktestPlugin.kt
+++ src/main/kotlin/doktest/DoktestPlugin.kt
@@ -7,26 +7,31 @@ import org.gradle.api.tasks.SourceSetContainer
 import org.gradle.api.tasks.testing.Test
 
 const val SOURCE_SET_NAME = "doktest"
+const val DOKTEST_TASK_NAME = "doktest"
 const val DOKTEST_TEST_TASK_NAME = "doktestTest"
 
 @Suppress("unused")
 class DoktestPlugin : Plugin<Project> {
     override fun apply(target: Project) {
+
         val sourceSets = target.extensions.getByType(SourceSetContainer::class.java)
         val testSet = sourceSets.getByName(SourceSet.TEST_SOURCE_SET_NAME)
-        val doktestSet = sourceSets.create(SOURCE_SET_NAME)
-        doktestSet.apply {
-            doktestSet.compileClasspath += testSet.compileClasspath
-            doktestSet.runtimeClasspath += testSet.runtimeClasspath
+        val doktestSet = sourceSets.create(SOURCE_SET_NAME).apply {
+            compileClasspath += testSet.compileClasspath
+            runtimeClasspath += testSet.runtimeClasspath
         }
-        val doktestTask = target.tasks.create("doktest", Doktest::class.java)
-        val compileKotlin = doktestSet.getCompileTaskName("kotlin")
-        val compileKotlinTask = target.tasks.getByName(compileKotlin)
+
+        val doktestTask = target.tasks.create(DOKTEST_TASK_NAME, Doktest::class.java)
+
+        val compileKotlinTaskName = doktestSet.getCompileTaskName("kotlin")
+        val compileKotlinTask = target.tasks.getByName(compileKotlinTaskName)
+
         val doktestTestTask = target.tasks.create(DOKTEST_TEST_TASK_NAME, Test::class.java).apply {
             testClassesDirs = doktestSet.output.classesDirs
             classpath = doktestSet.runtimeClasspath
             useJUnitPlatform()
         }
+
         doktestTask.finalizedBy(compileKotlinTask, doktestTestTask)
     }
 }
