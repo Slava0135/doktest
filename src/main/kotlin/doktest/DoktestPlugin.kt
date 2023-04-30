2293e1bee8557f4d1ccbd053aa4cf585c68b7ea5
Slava0135 <super.novalskiy_0135@inbox.ru>
2023-04-30 10:09:22 +0300

    expose test task for configuration

--- src/main/kotlin/doktest/DoktestPlugin.kt
+++ src/main/kotlin/doktest/DoktestPlugin.kt
@@ -4,16 +4,19 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.tasks.SourceSet
 import org.gradle.api.tasks.SourceSetContainer
+import org.gradle.api.tasks.TaskContainer
+import org.gradle.api.tasks.TaskProvider
 import org.gradle.api.tasks.testing.Test
 
 const val SOURCE_SET_NAME = "doktest"
+const val DOKTEST_TEST_TASK_NAME = "doktestTest"
 
 @Suppress("unused")
 class DoktestPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         val sourceSets = target.extensions.getByType(SourceSetContainer::class.java)
         val testSet = sourceSets.getByName(SourceSet.TEST_SOURCE_SET_NAME)
-        val doktestSet = sourceSets.create("doktest")
+        val doktestSet = sourceSets.create(SOURCE_SET_NAME)
         doktestSet.apply {
             doktestSet.compileClasspath += testSet.compileClasspath
             doktestSet.runtimeClasspath += testSet.runtimeClasspath
@@ -21,7 +24,7 @@ class DoktestPlugin : Plugin<Project> {
         val doktestTask = target.tasks.create("doktest", Doktest::class.java)
         val compileKotlin = doktestSet.getCompileTaskName("kotlin")
         val compileKotlinTask = target.tasks.getByName(compileKotlin)
-        val doktestTestTask = target.tasks.create("doktestTest", Test::class.java).apply {
+        val doktestTestTask = target.tasks.create(DOKTEST_TEST_TASK_NAME, Test::class.java).apply {
             testClassesDirs = doktestSet.output.classesDirs
             classpath = doktestSet.runtimeClasspath
             useJUnitPlatform()
@@ -29,3 +32,7 @@ class DoktestPlugin : Plugin<Project> {
         doktestTask.finalizedBy(compileKotlinTask, doktestTestTask)
     }
 }
+
+@Suppress("unused")
+val TaskContainer.doktestTest: TaskProvider<Test>
+    get() = this.named(DOKTEST_TEST_TASK_NAME, Test::class.java)
