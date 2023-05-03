e9a75e8b0ca28cd4f6809ff3671474ca89c4b13d
Slava0135 <super.novalskiy_0135@inbox.ru>
2023-05-03 09:56:13 +0300

    simple TestKit test

--- src/test/kotlin/doktest/DoktestKtTest.kt
+++ src/test/kotlin/doktest/DoktestKtTest.kt
@@ -5,6 +5,8 @@ import org.gradle.testkit.runner.TaskOutcome
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.io.TempDir
 import java.io.File
+import java.nio.file.Files
+import java.nio.file.Paths
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
@@ -15,6 +17,8 @@ class DoktestKtTest {
     private lateinit var settingsFile: File
     private lateinit var buildFile: File
 
+    private val mainSrc = "/src/main/kotlin"
+
     @BeforeEach
     fun setup() {
         settingsFile = File(testProjectDir, "settings.gradle.kts").apply {
@@ -23,10 +27,25 @@ class DoktestKtTest {
         buildFile = File(testProjectDir, "build.gradle.kts").apply {
             writeText(javaClass.getResource("/build/build.gradle.kts")!!.readText())
         }
+        Files.createDirectories(Paths.get(testProjectDir.path + mainSrc))
     }
 
     @Test
     fun `test empty`() {
+        val result = GradleRunner.create()
+            .withProjectDir(testProjectDir)
+            .withArguments(DOKTEST_TASK_NAME)
+            .withPluginClasspath()
+            .build()
+        assertEquals(TaskOutcome.SUCCESS, result.task(":$DOKTEST_TASK_NAME")!!.outcome)
+        assertTrue(result.tasks(TaskOutcome.FAILED).isEmpty())
+    }
+
+    @Test
+    fun `test simple`() {
+        File(testProjectDir, "$mainSrc/simple.kt").apply {
+            writeText(javaClass.getResource("/simple/simple.kt")!!.readText())
+        }
         val result = GradleRunner.create()
             .withProjectDir(testProjectDir)
             .withArguments(DOKTEST_TASK_NAME)
---
+++ src/test/resources/simple/simple.kt
@@ -0,0 +1,10 @@
+package simple
+
+/**
+ * ```kotlin doctest
+ * val a = 1
+ * val b = 2
+ * assertEquals(a + b, plus(a, b))
+ * ```
+ */
+fun plus(a: Int, b: Int) = a + b
