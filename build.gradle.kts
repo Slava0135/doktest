2c5a7f95340cdc8112d550146e02114b849b15b5
Slava0135 <super.novalskiy_0135@inbox.ru>
2023-05-04 14:09:41 +0300

    gradle plugin configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,12 @@
+@file:Suppress("UnstableApiUsage")
+
 plugins {
     kotlin("jvm") version "1.8.21"
     `java-gradle-plugin`
 }
 
 group = "com.github.slava0135"
-version = "1.0-SNAPSHOT"
+version = "0.1.0"
 
 repositories {
     mavenCentral()
@@ -24,9 +26,15 @@ kotlin {
 }
 
 gradlePlugin {
+    group = "com.github.slava0135"
+    website.set("https://github.com/Slava0135/doktest")
+    vcsUrl.set("https://github.com/Slava0135/doktest.git")
     plugins {
         create("doktest") {
             id = "com.github.slava0135.doktest"
+            displayName = "Plugin for testing code in KDoc comments"
+            description = "A plugin that helps you verify Kotlin code in KDoc comments"
+            tags.set(listOf("kotlin", "testing", "documentation", "docs"))
             implementationClass = "doktest.DoktestPlugin"
         }
     }
